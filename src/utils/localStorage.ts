989d0982134d0ed1d867dfc505c3148fd2fe5bc0
zahidmiran <zahidmiron599@gmail.com>
2023-10-19 21:36:45 +0600

    localstorage is modified for geting accesstoken

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,9 +1,60 @@
+// // export const setLocalStorage = (key: string, token: string) => {
+// //   if (!key || typeof window === "undefined") return "";
+// //   return localStorage?.setItem(key, token);
+// // };
+
+// // export const getFromLocalStorage = (key: string) => {
+// //   if (!key || typeof window === "undefined") return "";
+// //   return localStorage?.getItem(key);
+// // };
+// export const setLocalStorage = (key: string, token: string) => {
+
+//   if (!key || typeof window === "undefined") return "";
+//   return localStorage?.setItem(key, token);
+// };
+
+// export const getFromLocalStorage = (key: string) => {
+//   if (!key || typeof window === "undefined") return "";
+//   return localStorage?.getItem(key);
+// };
+
+// if (typeof window !== 'undefined') {
+//   // Access localStorage here
+// }
 export const setLocalStorage = (key: string, token: string) => {
-  if (!key || typeof window === "undefined") return "";
-  return localStorage?.setItem(key, token);
+  if (!key) {
+    console.error("Invalid key provided for setLocalStorage.");
+    return "";
+  }
+
+  if (typeof window === "undefined") {
+    console.error("Accessing localStorage in a server-side context.");
+    return "";
+  }
+
+  try {
+    localStorage.setItem(key, token);
+  } catch (error) {
+    console.error("Error setting item in localStorage:", error);
+    return "";
+  }
 };
 
 export const getFromLocalStorage = (key: string) => {
-  if (!key || typeof window === "undefined") return "";
-  return localStorage?.getItem(key);
+  if (!key) {
+    console.error("Invalid key provided for getFromLocalStorage.");
+    return "";
+  }
+
+  if (typeof window === "undefined") {
+    console.error("Accessing localStorage in a server-side context.");
+    return "";
+  }
+
+  try {
+    return localStorage.getItem(key);
+  } catch (error) {
+    console.error("Error getting item from localStorage:", error);
+    return "";
+  }
 };
