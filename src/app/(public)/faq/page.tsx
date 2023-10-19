e9cb775a2f35416ce4c056ad564c312005c961cd
zahidmiran <zahidmiron599@gmail.com>
2023-10-19 19:44:41 +0600

    faq options created

--- src/app/(public)/faq/page.tsx
+++ src/app/(public)/faq/page.tsx
@@ -1,63 +1,70 @@
-"use client";
-import { CaretRightOutlined } from "@ant-design/icons";
-import type { CSSProperties } from "react";
+// "use client";
+// import { CaretRightOutlined } from "@ant-design/icons";
+// import type { CSSProperties } from "react";
+// import React from "react";
+// import type { CollapseProps } from "antd";
+// import { Collapse, theme } from "antd";
+// import { useFaqsQuery } from "@/redux/api/faqApi";
+
+// const text = `
+//   A dog is a type of domesticated animal.
+//   Known for its loyalty and faithfulness,
+//   it can be found as a welcome guest in many households across the world.
+// `;
+
+// const getItems: (panelStyle: CSSProperties) => CollapseProps["items"] = (
+//   panelStyle
+// ) => [
+//   {
+//     key: "1",
+//     label: "This is panel header 1",
+//     children: <p>{text}</p>,
+//     style: panelStyle,
+//   },
+//   {
+//     key: "2",
+//     label: "This is panel header 2",
+//     children: <p>{text}</p>,
+//     style: panelStyle,
+//   },
+//   {
+//     key: "3",
+//     label: "This is panel header 3",
+//     children: <p>{text}</p>,
+//     style: panelStyle,
+//   },
+// ];
+
+// const Faqs: React.FC = () => {
+//   const { data, isLoading } = useFaqsQuery();
+
+//   const { token } = theme.useToken();
+
+//   const panelStyle: React.CSSProperties = {
+//     marginBottom: 24,
+//     background: token.colorFillAlter,
+//     borderRadius: token.borderRadiusLG,
+//     border: "none",
+//   };
+
+//   return (
+//     <Collapse
+//       bordered={false}
+//       defaultActiveKey={["1"]}
+//       expandIcon={({ isActive }) => (
+//         <CaretRightOutlined rotate={isActive ? 90 : 0} />
+//       )}
+//       style={{ background: token.colorBgContainer }}
+//       items={getItems(panelStyle)}
+//     />
+//   );
+// };
+
+// export default Faqs;
 import React from "react";
-import type { CollapseProps } from "antd";
-import { Collapse, theme } from "antd";
-import { useFaqsQuery } from "@/redux/api/faqApi";
 
-const text = `
-  A dog is a type of domesticated animal.
-  Known for its loyalty and faithfulness,
-  it can be found as a welcome guest in many households across the world.
-`;
-
-const getItems: (panelStyle: CSSProperties) => CollapseProps["items"] = (
-  panelStyle
-) => [
-  {
-    key: "1",
-    label: "This is panel header 1",
-    children: <p>{text}</p>,
-    style: panelStyle,
-  },
-  {
-    key: "2",
-    label: "This is panel header 2",
-    children: <p>{text}</p>,
-    style: panelStyle,
-  },
-  {
-    key: "3",
-    label: "This is panel header 3",
-    children: <p>{text}</p>,
-    style: panelStyle,
-  },
-];
-
-const Faqs: React.FC = () => {
-  const { data, isLoading } = useFaqsQuery();
-
-  const { token } = theme.useToken();
-
-  const panelStyle: React.CSSProperties = {
-    marginBottom: 24,
-    background: token.colorFillAlter,
-    borderRadius: token.borderRadiusLG,
-    border: "none",
-  };
-
-  return (
-    <Collapse
-      bordered={false}
-      defaultActiveKey={["1"]}
-      expandIcon={({ isActive }) => (
-        <CaretRightOutlined rotate={isActive ? 90 : 0} />
-      )}
-      style={{ background: token.colorBgContainer }}
-      items={getItems(panelStyle)}
-    />
-  );
+const page = () => {
+  return <div>This is a Faq Components</div>;
 };
 
-export default Faqs;
+export default page;
