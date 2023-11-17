0b0c00d010b55bcbdfb8d980b82c81fab1bcfb0c
zahidmiran <zahidmiron599@gmail.com>
2023-11-17 17:40:37 +0600

    booking page slot options updated

--- src/components/bookingPage/Slots.tsx
+++ src/components/bookingPage/Slots.tsx
@@ -1,9 +1,11 @@
 import { useAddBookingMutation } from "@/redux/api/bookingApi";
+import { useProfileQuery } from "@/redux/api/profileApi";
 import { getUserInfo, isLoggedIn } from "@/services/auth.service";
 import { responseMessage } from "@/utils/responseMessage";
-import { message } from "antd";
+import { Select, message } from "antd";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import Swal from "sweetalert2";
 import HCModal from "../ui/Modal/HCModal";
 
 /* eslint-disable react/no-unescaped-entities */
@@ -20,6 +22,16 @@ function Slots({ slots, data: serviceData, date }: props) {
   const { role, userId } = getUserInfo() as any;
   const [addBooking, { isLoading }] = useAddBookingMutation();
   const router = useRouter();
+  const [paymentMethodSelect, setPaymentMethodSelect] =
+    useState("cashOnDelivery");
+
+  // @ts-ignore
+  const { data: pData } = useProfileQuery();
+  const profileData = pData?.profileData;
+
+  const onChange = (value: string) => {
+    setPaymentMethodSelect(value);
+  };
 
   const bookingHandler = async (data: any) => {
     if (isLoading) {
@@ -27,70 +39,133 @@ function Slots({ slots, data: serviceData, date }: props) {
     }
 
     try {
-      console.log(serviceData);
       const bookingData = {
         date: date,
         userId: userId,
         slotId: data?.id,
         serviceId: serviceData.data?.id,
-        paymentMethod: "cashOnDelivery",
+        paymentMethod: paymentMethodSelect,
       };
-      console.log(bookingData);
-      const res = await addBooking(bookingData);
 
-      console.log(res);
+      const res = await addBooking(bookingData);
 
       if (res) {
         setOpen(false);
 
         responseMessage(res, "Service Booked Successfully");
         if ("data" in res && res.data) {
+          Swal.fire("Thank You", "Service Booked Successfully", "success");
           router.push("/my-bookings");
         }
       }
     } catch (err: any) {
-      //   console.error(err.message);
       message.error(err.message);
     }
   };
 
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 justify-center mx-auto gap-4">
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 justify-center mx-auto gap-6">
       {slots?.map((slot: any) => (
-        <div
-          key={slot?.id}
-          className="p-6 text-center bg-hcOrange-base rounded-lg text-white"
-        >
-          <p>Start Time: {slot.startTime}</p>
-          {/* <p>Team Name: Apollo</p> */}
-          {isUserLoggedIn && role === "customer" ? (
-            <button
-              className="text-white bg-green-600 px-16 py-2 mt-2 rounded-md"
-              onClick={() => {
-                setOpen(true);
-                setSlotData(slot);
-              }}
-            >
-              Book Now
-            </button>
-          ) : (
-            <button
-              disabled
-              className="text-white bg-green-600 px-16 py-2 mt-2 rounded-md"
-            >
-              Login first
-            </button>
-          )}
+        <div key={slot?.id} className="text-center">
+          <div className="relative group">
+            <div className="absolute -inset-1 bg-gradient-to-r from-teal-600 to-teal-700 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
+            <div className="relative px-2 py-6 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top justify-center space-x-6">
+              <div className="space-y-2 text-center">
+                <p className="mb-2">
+                  Start time:{" "}
+                  <span className="bg-orange-50 px-2 py-1 rounded-md">
+                    {slot?.startTime}
+                  </span>
+                </p>
+                <p className="mt-2 text-sm">
+                  Team:{" "}
+                  <span className="bg-teal-50 px-2 py-1 rounded-md">
+                    {slot?.serviceTeam?.teamName}
+                  </span>
+                </p>
+                {isUserLoggedIn && role === "customer" ? (
+                  <div>
+                    <button
+                      className="text-white bg-teal-700 px-6 py-2 mt-2 rounded-md text-sm"
+                      onClick={() => {
+                        setOpen(true);
+                        setSlotData(slot);
+                      }}
+                    >
+                      Book Now
+                    </button>
+                  </div>
+                ) : (
+                  <button
+                    disabled
+                    className="text-white bg-teal-700 px-16 py-2 mt-2 rounded-md"
+                  >
+                    Login first
+                  </button>
+                )}
+              </div>
+            </div>
+          </div>
         </div>
       ))}
 
       <HCModal
-        title="Book Service"
         isOpen={open}
         closeModal={() => setOpen(false)}
         handleOk={() => bookingHandler(slotData)}
+        confirmLoading={isLoading}
+        okText="Confirm Booking"
       >
-        <p className="my-5">Do you want to book this service?</p>
+        <div>
+          <h2 className="my-5 text-lg text-teal-700 font-bold">
+            Book {serviceData?.data?.service?.serviceName} service
+          </h2>
+          <div>
+            <p>Booking Date:</p>
+            <input
+              required
+              value={date}
+              // onChange={onChange}
+              className="w-full py-3 px-3 rounded-md border border-gray-100"
+              disabled
+            />
+          </div>
+          <div className="mt-4">
+            <p>Time:</p>
+            <input
+              placeholder="Give Your Feedback"
+              required
+              // @ts-ignore
+              value={slotData?.startTime}
+              // onChange={onChange}
+              disabled
+              className="w-full py-3 px-3 rounded-md border border-gray-100"
+            />
+          </div>
+          <div className="mt-4">
+            <p>Address:</p>
+            <input
+              placeholder="Give Your Feedback"
+              required
+              // value={review}
+              // onChange={onChange}
+              className="w-full py-3 px-3 rounded-md border border-gray-100"
+              value={profileData?.address}
+              disabled
+            />
+          </div>
+          <div className="mt-4 mb-4">
+            <p>Select Payment Method:</p>
+            <Select
+              onChange={onChange}
+              style={{ width: "100%" }}
+              options={[
+                { value: "cashOnDelivery", label: "Cash On Delivery" },
+                { value: "online", label: "Online" },
+              ]}
+            />
+          </div>
+        </div>
       </HCModal>
     </div>
   );
--- src/components/ui/Modal/HCModal.tsx
+++ src/components/ui/Modal/HCModal.tsx
@@ -1,14 +1,17 @@
-import { Modal } from 'antd';
-import { ReactElement, ReactNode } from 'react';
+import { Modal } from "antd";
+import { ReactElement, ReactNode } from "react";
 
 interface IModal {
   isOpen: boolean;
   closeModal: () => void;
-  title: string | ReactNode;
+  title?: string | ReactNode;
   children: ReactElement;
   handleOk?: () => void;
   showCancelButton?: boolean;
+  confirmLoading?: boolean;
   showOkButton?: boolean;
+  handleChange?: () => void;
+  okText?: string;
 }
 
 const HCModal = ({
@@ -17,26 +20,31 @@ const HCModal = ({
   title,
   children,
   handleOk,
+  confirmLoading,
   showCancelButton = true,
   showOkButton = true,
+  okText,
 }: IModal) => {
   return (
     <Modal
       title={title}
       open={isOpen}
+      okText={okText}
       onOk={handleOk}
+      centered
       onCancel={closeModal}
+      confirmLoading={confirmLoading}
       cancelButtonProps={{
         style: {
-          display: showCancelButton ? 'inline' : 'none',
-          backgroundColor: '#0f766e',
-          color: 'white',
+          display: showCancelButton ? "inline" : "none",
+          backgroundColor: "#0f766e",
+          color: "white",
         },
       }}
       okButtonProps={{
         style: {
-          display: showOkButton ? 'inline' : 'none',
-          backgroundColor: '#dc2626',
+          display: showOkButton ? "inline" : "none",
+          backgroundColor: "#dc2626",
         },
       }}
     >
